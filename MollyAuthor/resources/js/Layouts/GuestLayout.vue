d5782f44a6665cfa0be3c870c426daa9ac1dc604
thomasbockhorn <historian.thomas@gmail.com>
2023-05-06 13:52:04 -0500

    Added a navbar

--- MollyAuthor/resources/js/Layouts/GuestLayout.vue
+++ MollyAuthor/resources/js/Layouts/GuestLayout.vue
@@ -1,12 +1,17 @@
 <template>
     <div>
+        <FrontEndNavbar />
         <slot></slot>
     </div>
 </template>
 
 <script>
+import FrontEndNavbar from "@/Components/FrontEndNavbar/FrontEndNavbar.vue";
 export default {
     name: "GuestLayout",
+    components: {
+        FrontEndNavbar,
+    },
 };
 </script>
 
