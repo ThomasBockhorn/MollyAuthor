e6693108973f63328012b2c6dfb2f6b06d8b4ab6
thomasbockhorn <historian.thomas@gmail.com>
2023-05-14 18:43:57 -0500

    Created Links and the Bio page with web route

--- MollyAuthor/resources/js/Components/FrontEndNavbarMenu/FrontEndNavbarMenu.vue
+++ MollyAuthor/resources/js/Components/FrontEndNavbarMenu/FrontEndNavbarMenu.vue
@@ -1,21 +1,21 @@
 <template>
     <div class="hidden sm:ml-6 sm:block">
         <div class="flex space-x-4">
-            <a
-                href="#"
+            <Link
+                href="/"
                 class="text-black rounded-md px-3 py-2 text-sm font-medium hover:bg-gray-700 hover:text-white"
                 aria-current="page"
-                >Home</a
+                >Home</Link
             >
             <a
                 href="#"
                 class="text-black hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
                 >Books</a
             >
-            <a
-                href="#"
+            <Link
+                href="/bio"
                 class="text-black hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
-                >Bio</a
+                >Bio</Link
             >
             <a
                 href="#"
@@ -25,3 +25,13 @@
         </div>
     </div>
 </template>
+<script>
+import { Link } from "@inertiajs/vue3";
+
+export default {
+    name: "FrontEndNavbarMenu",
+    components: {
+        Link,
+    },
+};
+</script>
---
+++ MollyAuthor/resources/js/Pages/Bio.vue
@@ -0,0 +1,15 @@
+<template>
+    <GuestLayout> </GuestLayout>
+</template>
+
+<script>
+import GuestLayout from "@/Layouts/GuestLayout.vue";
+export default {
+    name: "Bio",
+    components: {
+        GuestLayout,
+    },
+};
+</script>
+
+<style></style>
