2dfd17c37deb8075bf238325aa73a27125cfb2f6
thomasbockhorn <historian.thomas@gmail.com>
2023-05-14 18:17:49 -0500

    Fixed footer to bottom

--- MollyAuthor/resources/js/Components/FrontEndFooter/FrontEndFooter.vue
+++ MollyAuthor/resources/js/Components/FrontEndFooter/FrontEndFooter.vue
@@ -1,5 +1,5 @@
 <template>
-    <footer class="bg-white">
+    <footer class="bg-white inset-x-0 bottom-0">
         <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
             <div class="relative flex h-16 items-start justify-between">
                 <a class="mt-5" href="https://www.thomasbockhorn.com"
