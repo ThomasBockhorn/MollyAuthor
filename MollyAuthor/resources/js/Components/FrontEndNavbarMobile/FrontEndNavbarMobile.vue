e14202d465ecc9dbd2ed9580050cf37b7e188ffa
thomasbockhorn <historian.thomas@gmail.com>
2023-05-14 19:45:56 -0500

    Added links to frontendnavbarmobile

--- MollyAuthor/resources/js/Components/FrontEndNavbarMobile/FrontEndNavbarMobile.vue
+++ MollyAuthor/resources/js/Components/FrontEndNavbarMobile/FrontEndNavbarMobile.vue
@@ -1,25 +1,35 @@
 <template>
     <div class="space-y-1 px-2 pb-3 pt-2">
-        <a
-            href="#"
+        <Link
+            href="/"
             class="text-black hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium"
             aria-current="page"
-            >Home</a
+            >Home</Link
         >
         <a
             href="#"
             class="text-black hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium"
             >Books</a
         >
-        <a
-            href="#"
+        <Link
+            href="/bio"
             class="text-black hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium"
-            >Bio</a
+            >Bio</Link
         >
-        <a
-            href="#"
+        <Link
+            href="/contact"
             class="text-black hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium"
-            >Contact</a
+            >Contact</Link
         >
     </div>
 </template>
+<script>
+import { Link } from "@inertiajs/vue3";
+
+export default {
+    name: "FrontEndNavbarMobileMenu",
+    components: {
+        Link,
+    },
+};
+</script>
