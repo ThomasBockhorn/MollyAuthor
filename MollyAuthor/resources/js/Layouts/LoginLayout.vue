b71bd3c9d345ee0a89b35fa3a64a2f5ae68bdbf6
thomasbockhorn <historian.thomas@gmail.com>
2023-05-18 20:42:42 -0500

    Styled login

---
+++ MollyAuthor/resources/js/Layouts/LoginLayout.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="flex flex-col sm:justify-center items-center pt-6 sm:pt-0">
+        <div
+            id="guest-layout"
+            class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg"
+        >
+            <slot />
+        </div>
+    </div>
+</template>
+<style scoped>
+#guest-layout::-webkit-scrollbar {
+    display: none;
+}
+</style>
