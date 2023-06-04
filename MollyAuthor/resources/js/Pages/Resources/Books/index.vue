a281dacadd81625d4b6e6ecbfe0455f8d4678c30
thomasbockhorn <historian.thomas@gmail.com>
2023-06-04 16:28:10 -0500

    Added the grid for books index

--- MollyAuthor/resources/js/Pages/Resources/Books/index.vue
+++ MollyAuthor/resources/js/Pages/Resources/Books/index.vue
@@ -1,6 +1,14 @@
 <template>
     <AuthenticatedLayout>
-        <h1>Books</h1>
+        <div class="m-5">
+            <h1 class="text-center text-xl">Books</h1>
+            <div class="grid sm:grid-cols-2 sm:gap-4 justify-items-center m-10">
+                <div>grid1</div>
+                <div>grid2</div>
+                <div>grid3</div>
+                <div>grid4</div>
+            </div>
+        </div>
     </AuthenticatedLayout>
 </template>
 
