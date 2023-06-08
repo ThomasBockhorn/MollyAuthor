5a76c16b0ff8639bf42ef5dc432f60715d2d8f40
thomasbockhorn <historian.thomas@gmail.com>
2023-06-07 22:13:44 -0500

    created a card for the backend user to show books

--- MollyAuthor/resources/js/Pages/Resources/Books/index.vue
+++ MollyAuthor/resources/js/Pages/Resources/Books/index.vue
@@ -4,32 +4,56 @@
             <h1 class="text-center text-2xl">Books</h1>
             <div class="grid sm:grid-cols-2 sm:gap-4 justify-items-center m-10">
                 <div
+                    class="max-w-sm bg-white border border-gray-200 rounded-lg shadow"
                     v-for="book in books"
                     :key="book.id"
-                    class="border-2 border-black rounded p-1"
                 >
-                    <img
-                        :src="'/storage/covers/' + book.cover"
-                        :alt="book.title"
-                        class="w-40 h-40"
-                    />
-                    <h2 class="text-xl">{{ book.title }}</h2>
-                    <p class="text-lg">{{ book.genre }}</p>
-                    <p class="text-lg">{{ book.description }}</p>
-                    <Link
-                        :href="route('books.edit', book.id)"
-                        class="bg-cyan-500 rounded p-1 border-2 border-black text-white hover:bg-cyan-200 hover:text-black"
-                    >
-                        Edit
-                    </Link>
-                    <form @submit.prevent="destroy(book.id)" class="inline">
-                        <button
-                            type="submit"
-                            class="bg-cyan-500 rounded p-1 border-2 border-black text-white hover:bg-cyan-200 hover:text-black"
+                    <div href="#">
+                        <img
+                            class="rounded-t-lg"
+                            :src="'/storage/covers/' + book.cover"
+                            :alt="book.title"
+                        />
+                    </div>
+                    <div class="p-5">
+                        <a href="#">
+                            <h5
+                                class="mb-2 text-2xl font-bold tracking-tight text-gray-900"
+                            >
+                                Noteworthy technology acquisitions 2021
+                            </h5>
+                        </a>
+                        <p class="mb-3 font-normal text-gray-700">
+                            Here are the biggest enterprise technology
+                            acquisitions of 2021 so far, in reverse
+                            chronological order.
+                        </p>
+                        <a
+                            href="#"
+                            class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                        >
+                            Read more
+                            <svg
+                                aria-hidden="true"
+                                class="w-4 h-4 ml-2 -mr-1"
+                                fill="currentColor"
+                                viewBox="0 0 20 20"
+                                xmlns="http://www.w3.org/2000/svg"
+                            >
+                                <path
+                                    fill-rule="evenodd"
+                                    d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                                    clip-rule="evenodd"
+                                ></path>
+                            </svg>
+                        </a>
+                        <a
+                            href="#"
+                            class="inline-flex items-center mx-3 px-3 py-2 text-sm font-medium text-center text-white bg-red-500 rounded-lg hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300"
                         >
                             Delete
-                        </button>
-                    </form>
+                        </a>
+                    </div>
                 </div>
             </div>
         </div>
@@ -39,11 +63,13 @@
 <script>
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
 import { Link } from "@inertiajs/vue3";
+import { onDeactivated } from "vue";
 
 export default {
     components: {
         AuthenticatedLayout,
         Link,
+        onDeactivated,
     },
     props: {
         books: Array,
