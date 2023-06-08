7066a4342b66f9139beeb51225c7c81e44912f98
thomasbockhorn <historian.thomas@gmail.com>
2023-06-07 23:19:58 -0500

    Added data to backend cards

--- MollyAuthor/resources/js/Pages/Resources/Books/index.vue
+++ MollyAuthor/resources/js/Pages/Resources/Books/index.vue
@@ -16,17 +16,19 @@
                         />
                     </div>
                     <div class="p-5">
-                        <a href="#">
-                            <h5
-                                class="mb-2 text-2xl font-bold tracking-tight text-gray-900"
-                            >
-                                Noteworthy technology acquisitions 2021
-                            </h5>
-                        </a>
+                        <h5
+                            class="mb-2 text-2xl font-bold tracking-tight text-gray-900"
+                        >
+                            {{ book.title }}
+                        </h5>
+                        <h5
+                            class="mb-2 text-2xl font-bold tracking-tight text-gray-900"
+                        >
+                            {{ book.genre }}
+                        </h5>
+
                         <p class="mb-3 font-normal text-gray-700">
-                            Here are the biggest enterprise technology
-                            acquisitions of 2021 so far, in reverse
-                            chronological order.
+                            {{ book.description }}
                         </p>
                         <a
                             href="#"
