0475d406555e83e3e88f867a6e7855814c042827
thomasbockhorn <historian.thomas@gmail.com>
2023-06-07 23:20:14 -0500

    added cards to frontend

--- MollyAuthor/resources/js/Pages/Books.vue
+++ MollyAuthor/resources/js/Pages/Books.vue
@@ -1,10 +1,57 @@
 <template>
     <GuestLayout>
-        <div class="grid sm:grid-cols-2 sm:gap-4 justify-items-center m-10">
-            <div>1</div>
-            <div>2</div>
-            <div>3</div>
-            <div>4</div>
+        <div class="m-5">
+            <h1 class="text-center text-2xl">Books</h1>
+            <div class="grid sm:grid-cols-2 sm:gap-4 justify-items-center m-10">
+                <div
+                    class="max-w-sm bg-white border border-gray-200 rounded-lg shadow"
+                    v-for="book in books"
+                    :key="book.id"
+                >
+                    <div href="#">
+                        <img
+                            class="rounded-t-lg"
+                            :src="'/storage/covers/' + book.cover"
+                            :alt="book.title"
+                        />
+                    </div>
+                    <div class="p-5">
+                        <h5
+                            class="mb-2 text-2xl font-bold tracking-tight text-gray-900"
+                        >
+                            {{ book.title }}
+                        </h5>
+                        <h5
+                            class="mb-2 text-2xl font-bold tracking-tight text-gray-900"
+                        >
+                            {{ book.genre }}
+                        </h5>
+
+                        <p class="mb-3 font-normal text-gray-700">
+                            {{ book.description }}
+                        </p>
+                        <a
+                            href="#"
+                            class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                        >
+                            Read more
+                            <svg
+                                aria-hidden="true"
+                                class="w-4 h-4 ml-2 -mr-1"
+                                fill="currentColor"
+                                viewBox="0 0 20 20"
+                                xmlns="http://www.w3.org/2000/svg"
+                            >
+                                <path
+                                    fill-rule="evenodd"
+                                    d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                                    clip-rule="evenodd"
+                                ></path>
+                            </svg>
+                        </a>
+                    </div>
+                </div>
+            </div>
         </div>
     </GuestLayout>
 </template>
@@ -15,6 +62,11 @@ export default {
     name: "Books",
     components: {
         GuestLayout,
+    },
+    props: {
+        books: {
+            type: Array,
+        },
     },
 };
 </script>
