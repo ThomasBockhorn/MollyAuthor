eaf010cde987c50470b1bbd2682a570774aab795
thomasbockhorn <historian.thomas@gmail.com>
2023-06-29 21:07:44 -0500

    Created Event table

--- MollyAuthor/resources/js/Pages/Resources/Events/index.vue
+++ MollyAuthor/resources/js/Pages/Resources/Events/index.vue
@@ -1,6 +1,36 @@
 <template>
     <AuthenticatedLayout>
-        <h1>Events</h1>
+        <div class="m-5">
+            <h1 class="text-center text-2xl">Events</h1>
+            <div class="mt-5">
+                <table class="mx-auto w-full">
+                    <tr>
+                        <th>Title</th>
+                        <th>Description</th>
+                        <th>Location</th>
+                        <th>Date</th>
+                        <th>Edit</th>
+                        <th>Delete</th>
+                    </tr>
+                    <tr v-for="event in events" :key="event.id">
+                        <td>{{ event.title }}</td>
+                        <td>{{ event.description }}</td>
+                        <td>{{ event.location }}</td>
+                        <td>{{ event.date }}</td>
+                        <td>
+                            <button class="rounded-full bg-green text-white">
+                                Edit
+                            </button>
+                        </td>
+                        <td>
+                            <button class="rounded-full bg-red text-white">
+                                Delete
+                            </button>
+                        </td>
+                    </tr>
+                </table>
+            </div>
+        </div>
     </AuthenticatedLayout>
 </template>
 
@@ -9,6 +39,11 @@ import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
 export default {
     components: {
         AuthenticatedLayout,
+    },
+    props: {
+        events: {
+            type: Object,
+        },
     },
 };
 </script>
