b3a172cfc46b63b438527a6944b72fff07fb60b5
thomasbockhorn <historian.thomas@gmail.com>
2023-07-22 13:47:13 -0500

    Deleted the title of book in index

--- MollyAuthor/resources/js/Pages/Resources/Books/index.vue
+++ MollyAuthor/resources/js/Pages/Resources/Books/index.vue
@@ -1,7 +1,6 @@
 <template>
     <AuthenticatedLayout>
         <div class="m-5">
-            <h1 class="text-center text-2xl">Books</h1>
             <div class="grid sm:grid-cols-2 sm:gap-4 justify-items-center m-10">
                 <div
                     class="max-w-sm bg-white border border-gray-200 rounded-lg shadow"
